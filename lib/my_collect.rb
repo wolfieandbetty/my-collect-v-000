23a095c0a354398d9929dc39034e7ef2ca737863
Cat Norris <cannenorris@yahoo.com>
2017-11-28 21:37:50 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,15 +2,11 @@ def my_collect (array=["tom","susie","fred"])
   i=0
   collection=[]
     while i<array.length
-      if array[i].strip.include?" "==true
-      array[i].split
-    else
-      array[i].upcase
-    end
-
-      yield collection<<(array[i])
+      collect << yield(array[i])
       i=i+1
     end
 
     collection
   end
+
+  
