e8846eed16f588e6335e07a0e54ddb30106b5a20
Cat Norris <cannenorris@yahoo.com>
2017-11-28 21:19:05 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect (array)
+  i=0
+  collection=[]
+    while i<array.length 
+      if array[i].strip.include?" "==true
+      array[i].split
+    else
+      array[i].upcase
+    end
+      collection<< yield(array[i])
+      i=i+1
+    end
+    collection
+  end
 
 
